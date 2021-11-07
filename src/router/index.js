b6e5e5e28783fc9f6b58777933ec4a038b5484ff
Joe Brundage <joebrundage@icloud.com>
2021-11-06 19:53:27 -0600

    fixed footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Stats from '../views/Stats.vue'
+import Records from '../views/Records.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/stats',
+    name: 'Stats',
+    component: Stats
+  },
+  {
+    path: '/records',
+    name: 'Records',
+    component: Records
   },
   {
     path: '/about',
